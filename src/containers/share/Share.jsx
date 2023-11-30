be1a7e914cdee1cadadd4cb637a2d9bf2c45f4f2
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2023-11-30 09:19:16 +0100

    Meida querry

--- src/containers/share/Share.jsx
+++ src/containers/share/Share.jsx
@@ -8,19 +8,24 @@ import './share.css'
 function Share() {
     const [showPopup, setShowPopup] = useState(false);
 
-    const handleButtonClick = () => {
-        setShowPopup(!showPopup);
+    const handleMouseEnter = () => {
+        setShowPopup(true);
     };
+
+    const handleMouseLeave = () => {
+        setShowPopup(false);
+    };
+
 
     return (
         <div className='card__article_info_user_share'>
-            {showPopup && <div className="card__article_info_user_share_popup" onClick={handleButtonClick}>
+            {showPopup && <div className="card__article_info_user_share_popup" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <p className="card__article_info_user_share_popup_text">Share</p>
-                <FontAwesomeIcon icon={faSquareFacebook} size="xl" />
-                <FontAwesomeIcon icon={faXTwitter} size="xl" />
-                <FontAwesomeIcon icon={faPinterest} size="xl" />
+                <FontAwesomeIcon icon={faSquareFacebook} size="2xl" />
+                <FontAwesomeIcon icon={faXTwitter} size="2xl" />
+                <FontAwesomeIcon icon={faPinterest} size="2xl" />
             </div>}
-            <FontAwesomeIcon className="card__article_info_user_share_icon" icon={faShare} onClick={handleButtonClick} size="xl" />
+            <FontAwesomeIcon className="card__article_info_user_share_icon" icon={faShare} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} size="xl" />
         </div>
     );
 }
